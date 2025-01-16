287c468224185bdb6bfda7d028f6bb4f4a12cf87
misna04 <misnasyari@gmail.com>
2025-01-17 07:28:02 +0800

    click address

--- app/page.tsx
+++ app/page.tsx
@@ -149,26 +149,17 @@ export default function Home() {
 	return (
 		<div className="relative w-full h-screen">
 			{/* Search bar for destination */}
-			<div
-				// className="absolute top-4 left-20 z-[1000] bg-white shadow-lg rounded-lg p-2 flex items-center"
-				className="absolute top-4 left-20 z-[1000] bg-white shadow-lg rounded-lg p-2 flex flex-col"
-			>
+			<div className="absolute top-4 left-20 z-[1000] bg-white shadow-lg rounded-lg p-2 flex flex-col">
 				<div className="flex items-center space-x-2">
 					<input
 						type="text"
 						value={searchQuery}
 						onChange={handleInputChange} // Handle input change with debounce
-						// onChange={(e) => {
-						// 	setSearchQuery(e.target.value)
-						// 	fetchSuggestions(e.target.value)
-						// }}
 						placeholder="Search for a location"
-						// className="p-2 border border-gray-300 rounded-lg flex-grow text-sm text-gray-800 outline-none focus:ring-2 focus:ring-blue-500"
 						className="p-2 border border-gray-300 rounded-lg text-sm text-gray-800 outline-none focus:ring-2 focus:ring-blue-500"
 					/>
 					<button
 						onClick={handleSearch}
-						// className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
 						className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
 					>
 						Search
@@ -192,6 +183,7 @@ export default function Home() {
 								onClick={() => {
 									setSearchQuery(item.display_name)
 									setPosition([parseFloat(item.lat), parseFloat(item.lon)])
+									setAddress(item.display_name)
 									setSuggestions([]) // Clear suggestions
 									if (mapRef.current) {
 										mapRef.current.setView([parseFloat(item.lat), parseFloat(item.lon)], 15, { animate: true })
