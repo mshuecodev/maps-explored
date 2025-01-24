6f54c9e0fc26c36282b377fc3a8c7bf68e92bb71
misna04 <misnasyari@gmail.com>
2025-01-24 17:17:17 +0800

    yml test

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	basePath: process.env.NEXT_PUBLIC_BASE_PATH || "", // Use environment variable or default to ""
-	assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || ""
+	// basePath: process.env.NEXT_PUBLIC_BASE_PATH || "", // Use environment variable or default to ""
+	// assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || ""
 }
 
 export default nextConfig
