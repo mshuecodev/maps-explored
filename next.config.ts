5bacd776d5681ad62bbd8083eb80a09e2db76613
misna04 <misnasyari@gmail.com>
2025-01-24 13:24:21 +0800

    route config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	basePath: "/maps-explored" // Replace 'maps-explored' with your GitHub repository name
 }
 
 export default nextConfig
